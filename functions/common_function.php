920929a32172755afdabb4f11a80d5a31322f387
ayushmakafle <amikafle1@gmail.com>
2023-06-20 19:33:46 +0545

    get ip address

--- functions/common_function.php
+++ functions/common_function.php
@@ -268,4 +268,26 @@ function view_details(){
 }
 }
 }
+
+//get ip 
+function getIPAddress() {  
+    //whether ip is from the share internet  
+     if(!empty($_SERVER['HTTP_CLIENT_IP'])) {  
+                $ip = $_SERVER['HTTP_CLIENT_IP'];  
+        }  
+    //whether ip is from the proxy  
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {  
+                $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];  
+     }  
+//whether ip is from the remote address  
+    else{  
+             $ip = $_SERVER['REMOTE_ADDR'];  
+     }  
+     return $ip;  
+}  
+//$ip = getIPAddress();  
+// echo 'User Real IP Address - '.$ip;  
+
+//cart function
+
 ?>
